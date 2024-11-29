5ee51fd53fde492c7d722a37a17c3b831b81c022
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2024-11-29 01:40:59 +0100

    Make coord precision a slider
    and put arguments on separate lines

--- src/main/java/me/oondanomala/fpkmod/config/Config.java
+++ src/main/java/me/oondanomala/fpkmod/config/Config.java
@@ -3,6 +3,7 @@ package me.oondanomala.fpkmod.config;
 import me.oondanomala.fpkmod.FPKMod;
 import me.oondanomala.fpkmod.util.TextUtil;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.client.config.GuiConfigEntries;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -28,15 +29,52 @@ public class Config {
         // Hacky, but it works!
         String[] colorNames = {"§0Black", "§1Dark Blue", "§2Dark Green", "§3Dark Aqua", "§4Dark Red", "§5Dark Purple", "§6Gold", "§7Gray", "§8Dark Gray", "§9Blue", "§aGreen", "§bAqua", "§cRed", "§dLight Purple", "§eYellow", "§fWhite"};
 
-        prefix = configuration.getString("Prefix", Configuration.CATEGORY_CLIENT, "<FPK>", "The prefix that will be used in chat messages.");
-        color1 = configuration.getString("Primary Color", Configuration.CATEGORY_CLIENT, "§6Gold", "The main color used in text.", colorNames).substring(0, 2);
-        color2 = configuration.getString("Secondary Color", Configuration.CATEGORY_CLIENT, "§fWhite", "The secondary color used in text.", colorNames).substring(0, 2);
-
-        enableLabels = configuration.getBoolean("Enable Labels", Configuration.CATEGORY_CLIENT, true, "Whether to show labels at all.");
-        doublePrecision = configuration.getInt("Coord Precision", Configuration.CATEGORY_CLIENT, 6, 0, 16, "The number of decimals shown in floating point numbers.");
-        trimZeroes = configuration.getBoolean("Trim Zeroes", Configuration.CATEGORY_CLIENT, false, "Whether to remove any trailing zeroes from labels.");
-
-        mpkCommand = configuration.get(Configuration.CATEGORY_CLIENT, "MPK Command", true, "Whether to create the \"mpk\" alias to the \"fpk\" command.").setRequiresMcRestart(true).getBoolean();
+        prefix = configuration.getString(
+                "Prefix",
+                Configuration.CATEGORY_CLIENT,
+                "<FPK>",
+                "The prefix that will be used in chat messages."
+        );
+        color1 = configuration.getString(
+                "Primary Color",
+                Configuration.CATEGORY_CLIENT,
+                "§6Gold",
+                "The main color used in text.",
+                colorNames
+        ).substring(0, 2);
+        color2 = configuration.getString(
+                "Secondary Color",
+                Configuration.CATEGORY_CLIENT,
+                "§fWhite",
+                "The secondary color used in text.",
+                colorNames
+        ).substring(0, 2);
+        enableLabels = configuration.getBoolean(
+                "Enable Labels",
+                Configuration.CATEGORY_CLIENT,
+                true,
+                "Whether to show labels at all."
+        );
+        doublePrecision = configuration.get(
+                Configuration.CATEGORY_CLIENT,
+                "Coord Precision",
+                6,
+                "The number of decimals shown in floating point numbers.",
+                0,
+                16
+        ).setConfigEntryClass(GuiConfigEntries.NumberSliderEntry.class).getInt();
+        trimZeroes = configuration.getBoolean(
+                "Trim Zeroes",
+                Configuration.CATEGORY_CLIENT,
+                false,
+                "Whether to remove trailing zeroes from floating point numbers."
+        );
+        mpkCommand = configuration.get(
+                Configuration.CATEGORY_CLIENT,
+                "MPK Command",
+                true,
+                "Whether to create the \"mpk\" alias to the \"fpk\" command."
+        ).setRequiresMcRestart(true).getBoolean();
 
         TextUtil.setDecimalPrecision(doublePrecision, trimZeroes);
 
