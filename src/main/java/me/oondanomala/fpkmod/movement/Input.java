c1409ea08bc5cc2d8bc22f49a8b99a36bd9a5348
ssnak <sergregarias@gmail.com>
2025-03-28 17:08:58 -0400

    style: 0.0f to 0f
    Signed-off-by: ssnak <sergregarias@gmail.com>

--- src/main/java/me/oondanomala/fpkmod/movement/Input.java
+++ src/main/java/me/oondanomala/fpkmod/movement/Input.java
@@ -7,23 +7,31 @@ public class Input {
     public boolean jumping = false;
     public boolean sneaking = false;
     public int duration = 1;
-    public Input(){}
+
+    public Input() {
+    }
+
     public void walk() {
         this.sprinting = false;
     }
+
     public void jump() {
         this.jumping = true;
     }
+
     public void sneak() {
         this.sneaking = true;
     }
+
     public void move(float forwards, float sideways) {
         this.forwardMove = forwards;
         this.strafeMove = sideways;
     }
+
     public boolean isMoving() {
-        return forwardMove > 0.0f || (strafeMove > 0.0f);
+        return forwardMove > 0f || (strafeMove > 0f);
     }
+
     public boolean isEqual(Input other) {
         return forwardMove == other.forwardMove && strafeMove == other.strafeMove && sprinting == other.sprinting && jumping == other.jumping && sneaking == other.sneaking;
     }
