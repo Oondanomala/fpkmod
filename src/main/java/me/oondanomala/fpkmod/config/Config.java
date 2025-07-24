b52644fc2fed91197686fa6560f5f11170ba1e3e
Oondanomala <87101222+Oondanomala@users.noreply.github.com>
2025-07-24 20:41:25 +0200

    Add helper methods for creating config options
    Maybe a bit overkill, but I don't want to have to deal with Forge's shitty awful config system again
    Why is this API so inconsistent I swear can't it just be normal
    What do you mean you have getFloat() but not getDouble() but you have get(double) and not get(float)?? And of course getFloat() just treats the config value as a string making it completely and utterly useless. Why does it even exist. I know Minecraft's and Forge's code isn't very good but wow this is truly next level.
    Congrats guys.
    I wish I had 1.12.2's annotation based system.
    And even then it would probably still suck.
    
    We should all migrate to 1.13.
    Then every problem would be solved.
    
    Hate. Hate. Hate.

--- src/main/java/me/oondanomala/fpkmod/config/Config.java
+++ src/main/java/me/oondanomala/fpkmod/config/Config.java
@@ -2,6 +2,7 @@ package me.oondanomala.fpkmod.config;
 
 import me.oondanomala.fpkmod.FPKMod;
 import me.oondanomala.fpkmod.util.TextUtil;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.client.config.GuiConfigEntries;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
@@ -34,48 +35,37 @@ public class Config {
     }
 
     public void loadConfig() {
-        // Hacky, but it works!
-        final String[] colorNames = {"§0Black", "§1Dark Blue", "§2Dark Green", "§3Dark Aqua", "§4Dark Red", "§5Dark Purple", "§6Gold", "§7Gray", "§8Dark Gray", "§9Blue", "§aGreen", "§bAqua", "§cRed", "§dLight Purple", "§eYellow", "§fWhite"};
-
-        prefix = configuration.getString(
+        prefix = getStringConfig(
                 "Prefix",
-                Configuration.CATEGORY_CLIENT,
-                "<FPK>",
-                "The prefix that will be used in chat messages."
+                "The prefix that will be used in chat messages.",
+                "<FPK>"
         );
-        color1 = configuration.getString(
+        color1 = getColorConfig(
                 "Primary Color",
-                Configuration.CATEGORY_CLIENT,
-                "§6Gold",
                 "The main color used in text.",
-                colorNames
-        ).substring(0, 2);
-        color2 = configuration.getString(
-                "Secondary Color",
-                Configuration.CATEGORY_CLIENT,
-                "§fWhite",
-                "The secondary color used in text.",
-                colorNames
-        ).substring(0, 2);
-        enableLabels = configuration.getBoolean(
-                "Enable Labels",
-                Configuration.CATEGORY_CLIENT,
-                true,
-                "Whether to show labels at all."
+                EnumChatFormatting.GOLD
         );
-        doublePrecision = configuration.get(
-                Configuration.CATEGORY_CLIENT,
+        color2 = getColorConfig(
+                "Secondary Color",
+                "The secondary color used in text.",
+                EnumChatFormatting.WHITE
+        );
+        enableLabels = getBooleanConfig(
+                "Enable Labels",
+                "Whether to show labels at all.",
+                true
+        );
+        doublePrecision = getIntSliderConfig(
                 "Coord Precision",
-                6,
                 "The number of decimals shown in floating point numbers.",
+                6,
                 0,
                 16
-        ).setConfigEntryClass(GuiConfigEntries.NumberSliderEntry.class).getInt();
-        trimZeroes = configuration.getBoolean(
+        );
+        trimZeroes = getBooleanConfig(
                 "Trim Zeroes",
-                Configuration.CATEGORY_CLIENT,
-                false,
-                "Whether to remove trailing zeroes from floating point numbers."
+                "Whether to remove trailing zeroes from floating point numbers.",
+                false
         );
         mpkCommand = configuration.get(
                 Configuration.CATEGORY_CLIENT,
@@ -83,32 +73,28 @@ public class Config {
                 true,
                 "Whether to create the \"mpk\" alias to the \"fpk\" command."
         ).setRequiresMcRestart(true).getBoolean();
-        sendOffsetInChat = configuration.getBoolean(
+        sendOffsetInChat = getBooleanConfig(
                 "Send Offsets In Chat",
-                Configuration.CATEGORY_CLIENT,
-                false,
-                "Whether to send the land offsets in chat when landing on a landing block."
+                "Whether to send the land offsets in chat when landing on a landing block.",
+                false
         );
-        ignoreZeroOffsets = configuration.getBoolean(
+        ignoreZeroOffsets = getBooleanConfig(
                 "Ignore -0 Offsets",
-                Configuration.CATEGORY_CLIENT,
-                false,
-                "Whether to not count -0 offsets as PBs."
+                "Whether to not count -0 offsets as PBs.",
+                false
         );
-        enableAntiCP = configuration.getBoolean(
+        enableAntiCP = getBooleanConfig(
                 "AntiCP",
-                Configuration.CATEGORY_CLIENT,
-                false,
-                "Whether to block right-clicks after landing for a specified amount of time."
+                "Whether to block right-clicks after landing for a specified amount of time.",
+                false
         );
-        antiCPTime = (int) (configuration.get(
-                Configuration.CATEGORY_CLIENT,
+        antiCPTime = (int) (getDoubleConfig(
                 "AntiCP Time",
-                2d,
                 "How long to block right-clicks for, in seconds.",
-                0d,
-                20d
-        ).getDouble() * 1000);
+                2,
+                0,
+                20
+        ) * 1000);
 
         sprintToggled = configuration.get(Configuration.CATEGORY_CLIENT, "togglesprint", false).setShowInGui(false).getBoolean();
         renderLandingBox = configuration.get(Configuration.CATEGORY_CLIENT, "renderLB", true).setShowInGui(false).getBoolean();
@@ -118,6 +104,87 @@ public class Config {
 
         configuration.save();
     }
+
+    /**
+     * Creates a {@link String} config option,
+     * and returns its value.
+     *
+     * @param name         The name of the option, this is also used as an identifier
+     * @param description  A description of what the option does
+     * @param defaultValue The value to use if the option did not previously exist
+     * @return The config value if it already exists, otherwise {@code defaultValue}
+     */
+    private String getStringConfig(String name, String description, String defaultValue) {
+        return configuration.get(Configuration.CATEGORY_CLIENT, name, defaultValue, description).getString();
+    }
+
+    /**
+     * Creates a <tt>boolean</tt> config option,
+     * and returns its value.
+     *
+     * @param name         The name of the option, this is also used as an identifier
+     * @param description  A description of what the option does
+     * @param defaultValue The value to use if the option did not previously exist
+     * @return The config value if it already exists, otherwise {@code defaultValue}
+     */
+    private boolean getBooleanConfig(String name, String description, boolean defaultValue) {
+        return configuration.get(Configuration.CATEGORY_CLIENT, name, defaultValue, description).getBoolean();
+    }
+
+    /**
+     * Creates an <tt>int</tt> config option with a minimum and a maximum allowed value,
+     * and returns its value.
+     * It will be shown in the configuration GUI as a slider.
+     *
+     * @param name         The name of the option, this is also used as an identifier
+     * @param description  A description of what the option does
+     * @param defaultValue The value to use if the option did not previously exist
+     * @param minValue     The minimum value the option can be set to
+     * @param maxValue     The maximum value the option can be set to
+     * @return The config value if it already exists, otherwise {@code defaultValue}
+     */
+    private int getIntSliderConfig(String name, String description, int defaultValue, int minValue, int maxValue) {
+        return configuration.get(Configuration.CATEGORY_CLIENT, name, defaultValue, description, minValue, maxValue)
+                .setConfigEntryClass(GuiConfigEntries.NumberSliderEntry.class).getInt();
+    }
+
+    /**
+     * Creates a <tt>double</tt> config option with a minimum and a maximum allowed value,
+     * and returns its value.
+     *
+     * @param name         The name of the option, this is also used as an identifier
+     * @param description  A description of what the option does
+     * @param defaultValue The value to use if the option did not previously exist
+     * @param minValue     The minimum value the option can be set to
+     * @param maxValue     The maximum value the option can be set to
+     * @return The config value if it already exists, otherwise {@code defaultValue}
+     */
+    private double getDoubleConfig(String name, String description, double defaultValue, double minValue, double maxValue) {
+        return configuration.get(Configuration.CATEGORY_CLIENT, name, defaultValue, description, minValue, maxValue).getDouble();
+    }
+
+    /**
+     * Creates a color picker config option,
+     * allowing the user to choose between all color formatting codes.
+     * The color is returned as a 2 character string composed of the
+     * control character ({@code §}) and the formatting character,
+     * so that it can be easily used in text.
+     *
+     * @param name         The name of the option, this is also used as an identifier
+     * @param description  A description of what the option does
+     * @param defaultValue The value to use if the option did not previously exist
+     * @return A 2 character string representing the config value if it already exists,
+     *         otherwise the 2 character string representing {@code defaultValue}
+     * @see EnumChatFormatting
+     */
+    private String getColorConfig(String name, String description, EnumChatFormatting defaultValue) {
+        if (!defaultValue.isColor() || defaultValue.ordinal() > COLOR_NAMES.length - 1) {
+            throw new IllegalArgumentException("The default value must be a valid color");
+        }
+        return configuration.get(Configuration.CATEGORY_CLIENT, name, COLOR_NAMES[defaultValue.ordinal()], description, COLOR_NAMES).getString().substring(0, 2);
+    }
+
+    private static final String[] COLOR_NAMES = {"§0Black", "§1Dark Blue", "§2Dark Green", "§3Dark Aqua", "§4Dark Red", "§5Dark Purple", "§6Gold", "§7Gray", "§8Dark Gray", "§9Blue", "§aGreen", "§bAqua", "§cRed", "§dLight Purple", "§eYellow", "§fWhite"};
 
     /**
      * Sets the provided config option to {@code value}.
